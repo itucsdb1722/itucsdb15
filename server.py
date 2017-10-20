9cf4c316285ca60798cedaf02dee768c62b69f6f
AydosIlgazi <aydosilgazi@gmail.com>
2017-10-20 21:17:51 +0300

    0.0.0.0

--- server.py
+++ server.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-    app.run(host='127.0.0.1', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port, debug=debug)
